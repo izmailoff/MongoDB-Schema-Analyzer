a7158ce78085fe2760078495bf179c5f370653bc
Aleksey Izmailov <aleksey.izmailov@sap.com>
2018-01-29 10:03:43 +0800

    Silence mongo driver log messages up to ERROR level. Check connectivity on init. Don't fail if default props doesn't exist. This is a usable version now but improvements can be made.

--- src/main/scala/me/izmailoff/MongoConfig.scala
+++ src/main/scala/me/izmailoff/MongoConfig.scala
@@ -1,18 +1,21 @@
 package wws.db.connection
 
-import java.io.FileInputStream
+import java.io.{File, FileInputStream}
 
 import com.mongodb.{MongoClient, MongoException, ServerAddress}
 import net.liftweb.common.Full
 import net.liftweb.mongodb._
 import net.liftweb.util.{DefaultConnectionIdentifier, Props}
 
+import scala.util.{Failure, Success, Try}
+
 /**
  * MongoDb connection provider.
  */
 object MongoConfig {
   val filename = "default.props"
-  Props.whereToLook = () => ((filename, () => Full(new FileInputStream(filename))) :: Nil)
+  if (new File(filename).exists)
+    Props.whereToLook = () => ((filename, () => Full(new FileInputStream(filename))) :: Nil)
 
   val hostname = Props.get("mongo.host", "127.0.0.1")
   val port = Props.getInt("mongo.port", 27017)
@@ -21,26 +24,25 @@ object MongoConfig {
   /**
    * Define default Mongo connection provider based on props.
    */
-  def init() {
+  def init(): Unit = {
     val server = new ServerAddress(hostname, port)
     MongoDB.defineDb(DefaultConnectionIdentifier, new MongoClient(server), dbName)
+    if(!isConnected())
+      sys.exit(1)
   }
 
   /**
    * Checks if there is connectivity to MongoDB server by
    * checking default DB connection and calling some db command.
    */
-  def isConnected = {
-    val db = MongoDB.getDb(DefaultConnectionIdentifier)
-    if (db.isEmpty)
+  def isConnected() = Try {
+    getDb.map(_.getStats()).isDefined
+  } match {
+    case Success(dbDefined) =>
+      dbDefined
+    case Failure(err) =>
+      println(s"Could NOT connect to DB. Got: $err")
       false
-    else
-      try {
-        db.get.getStats()
-        true
-      } catch {
-        case _: MongoException => false
-      }
   }
   
   def getDb = MongoDB.getDb(DefaultConnectionIdentifier)
